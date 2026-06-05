5084aaf2cf5e0b6357e9fe68d0a63b59b13a0be1
Vladislav Kochenkov <priznavologia@gmail.com>
2026-06-05 20:07:45 +0300

    update lua

--- SandBox/assets/scripts/game.lua
+++ SandBox/assets/scripts/game.lua
@@ -27,10 +27,13 @@ function OnStart()
      Log("Game script started")
     local scene = GetScene()
     Log("Scene obtained")
+
+    -- Player
     local player = scene:CreateGameObject("Player")
     Log("Player created")
     local transform = player:AddComponent_Transform()
     transform:SetPosition(0, 0, 0)
+    transform:SetScale(2.0, 2.0)
     Log("Transform added")
     local renderer = player:AddComponent_SpriteRenderer()
     Log("SpriteRenderer added")
@@ -41,54 +44,61 @@ function OnStart()
     renderer:SetShader(shader)
     renderer:SetTexture(texture)
     Log("Shader and Texture set")
-    
+
       -- UI: текст
     local font = LoadFont("assets/fonts/arial.ttf")  -- убедись, что шрифт лежит здесь
      Log("Font loaded: " .. tostring(font))
     if font then
-        Log("Font is valid")
         local textObj = scene:CreateUIObject("WelcomeText")
-        Log("Text object created")
+
         local tt = textObj:AddComponent_Transform()
-        tt:SetPosition(50, 50, 0)
+
+        local anchor = textObj:AddComponent_Anchor()
+        anchor:SetPreset(1)
+        anchor:SetSize(200, 50)
+
         local tr = textObj:AddComponent_TextRenderer()
-        Log("TextRenderer added")
         tr:SetFont(font)
-        Log("Font set")
+        tr:SetFontSize(64)
         tr:SetText("Hello, UI!")
-        Log("Text set")
         tr:SetColor(255, 215, 0, 255) -- золотой
-        Log("Color set")
+        tr:SetAlignment(0.4, 0.5)
     else
         Log("Failed to load font")
     end
 
      -- UI: кнопка
     local btnObj = scene:CreateUIObject("TestButton")
-    Log("Button object created")
+    btnObj:SetZOrder(2)  -- ниже текста
     local bt = btnObj:AddComponent_Transform()
-    bt:SetPosition(200, 200, 0)
+
+    local anchor = btnObj:AddComponent_Anchor()
+    anchor:SetScreenAnchor(0.5, 0.5) -- центр экрана
+    anchor:SetObjectAnchor(0.5, 0.5) -- центр объекта
+    anchor:SetPreset(8)
+    anchor:SetSize(200, 50)
+
     local bgr = btnObj:AddComponent_SpriteRenderer()
-    -- чтобы увидеть кнопку даже без текстуры фона, зададим ей цветной прямоугольник? Но SpriteRenderer не умеет цвет без текстуры.
-    -- Поэтому временно зададим любую существующую текстуру, например test.png
-    bgr:SetTexture(GetTexture("assets/textures/test.png"))
-    Log("Button background set")
+    bgr:SetShader(GetShader("SpriteShader", vertexSrc, fragmentSrc))
+    bgr:SetTexture(GetTexture("assets/ui/button.png"))
+    bgr:SetIsUI(true)
+    bgr:SetAlignment(0.5, 0.5)
+
     local btr = btnObj:AddComponent_TextRenderer()
     if font then
         btr:SetFont(font)
         btr:SetText("Click me")
         btr:SetColor(255, 255, 255, 255)
-        Log("Button text set")
-    else
-        Log("Font not available for button text")
+        btr:SetFontSize(42)   -- добавьте эту строку, чтобы текст был читаемым
+        btr:SetAlignment(0.65, 0.6)
     end
+
     local button = btnObj:AddComponent_Button()
     button:SetSize(200, 50)
     button:SetCallback(function() Log("Button clicked!") end)
-    Log("Button setup done")
 
-    Log("Scene setup finished")
-end
+        Log("Scene setup finished")
+    end
 
 function OnUpdate(dt)
     if IsKeyDown(32) then -- пробел
